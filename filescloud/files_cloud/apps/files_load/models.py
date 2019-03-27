9d024e63bf986f665eb447f8d9ee2098df916a1f
OcaenEyes <1450136519@qq.com>
2019-03-27 23:19:06 +0800

    files_upload

--- filescloud/files_cloud/apps/files_load/models.py
+++ filescloud/files_cloud/apps/files_load/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Upload(models.Model):
+    DownloadCount = models.IntegerField(verbose_name="访问次数",default=0)
+    code = models.CharField(max_length=8,verbose_name="code")
+    path = models.CharField(max_length=32,verbose_name="下载路径")
+    name = models.CharField(max_length=32,verbose_name="文件名",default="")
+    FileSize = models.CharField(max_length=10,verbose_name="文件大小")
+    PCIP = models.CharField(max_length=32,verbose_name="IP地址",default="")
+
+    class Meta():
+        verbose_name = "download"
+        db_table = "down_load"
+
+    def __str__(self):
+        return self.name
--- filescloud/files_cloud/apps/files_load/views.py
+++ filescloud/files_cloud/apps/files_load/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
 # Create your views here.
 def files_upload(request):
-    return render(request,'files_upload.html')
+    if request.method == 'GET':
+        return render(request,'files_upload.html')
+
+    if request.method == 'POST':
+        file = request.Files.get('upload')
+        if not file:
+            return HttpResponse("no files for upload")
+
 
 def files_download(request):
     return render(request,'files_download.html')
