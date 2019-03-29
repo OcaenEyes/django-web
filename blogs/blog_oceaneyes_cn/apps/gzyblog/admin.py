e81300c5f08b636cc76fe887bf1c2bdee6675712
OcaenEyes <1450136519@qq.com>
2019-03-29 21:42:57 +0800

    blogs

--- blogs/blog_oceaneyes_cn/apps/gzyblog/admin.py
+++ blogs/blog_oceaneyes_cn/apps/gzyblog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Tag,Category,Article,AdminUser
 # Register your models here.
+
+class TagManage(admin.ModelAdmin):
+    list_display = ('id','tag')
+
+class CategoryManage(admin.ModelAdmin):
+    list_display = ('id','category')
+
+class ArticleManage(admin.ModelAdmin):
+    list_display = ('id','title','content')
+
+class AdminUserManage(admin.ModelAdmin):
+    list_display = ('id','admin_user')
+
+admin.site.register(Tag,TagManage)
+admin.site.register(Category,CategoryManage)
+admin.site.register(Article,ArticleManage)
+admin.site.register(AdminUser,AdminUserManage)
--- blogs/blog_oceaneyes_cn/apps/gzyblog/models.py
+++ blogs/blog_oceaneyes_cn/apps/gzyblog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    id = models.IntegerField(primary_key=id)
+    tag = models.CharField(max_length=32,verbose_name="标签管理")
+
+class Category(models.Model):
+    id = models.IntegerField(primary_key=id)
+    category = models.CharField(max_length=32,verbose_name="分类管理")
+
+class Article(models.Model):
+    id = models.IntegerField(primary_key=id)
+    title = models.CharField(default='',max_length=20,verbose_name="文章标题")
+    content = models.TextField(default='',verbose_name="文章内容")
+
+class AdminUser(models.Model):
+    id = models.IntegerField(primary_key=id)
+    admin_user = models.TextField(verbose_name="文章管理")
